5f46cf64b8429b4df28c32d99a245cf58e6c9941
Goz <gozdeozcan13@gmail.com>
2021-04-02 15:15:08 +0200

    Add initial project

---
+++ DigitalInvest.FundingPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Ev.DigitalInvest.FundingPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
